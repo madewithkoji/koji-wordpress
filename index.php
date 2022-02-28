0887fd18c8ee36380aa4cbbb0e06292ac36a194f
sean <admin@koji-projects.com>
2022-02-28 10:21:55 -0800

    Remove Gutenberg from plugin name

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Plugin Name:       Koji Block - Gutenberg Block for Koji Apps
+ * Plugin Name:       Koji Block - Embed Block for Koji Apps
  * Plugin URI:        https://withkoji.com/
  * Description:       Easily embed Koji Apps in Wordpress posts. Supercharge your posts with e-commerce apps, games, and other types of powerful interactive posts.
  * Version:           1.0.0
