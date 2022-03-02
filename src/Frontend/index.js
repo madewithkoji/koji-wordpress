ea7744d47a1853f9b4b1e271ae55e43b281eb543
Victor Demin <demmbox@gmail.com>
2022-03-02 12:25:24 +0400

    global koji links handler

--- src/Frontend/index.js
+++ src/Frontend/index.js
@@ -1,22 +1,49 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import KojiAppModal from "./KojiAppModal"
+import { isURLValid, getKojiApp } from "../Utils/validation"
 
-document.addEventListener("DOMContentLoaded", () => {
+const loadKojiApp = (url) => {
+  const container = document.createElement("div")
+
+  ReactDOM.render(
+    <KojiAppModal url={url} onClose={() => container.remove()} />,
+    document.body.appendChild(container)
+  )
+}
+
+const loadKojiButtons = () => {
   const buttons = document.querySelectorAll(".koji-embed-button")
-
-  const loadKojiApp = (url) => {
-    const container = document.createElement("div")
-
-    ReactDOM.render(
-      <KojiAppModal url={url} onClose={() => container.remove()} />,
-      document.body.appendChild(container)
-    )
-  }
 
   buttons.forEach((item) => {
     item.addEventListener("click", () => {
       loadKojiApp(item.dataset.koji)
     })
   })
+}
+
+const loadKojiLinks = () => {
+  const links = document.querySelectorAll("a")
+
+  links.forEach((item) => {
+    const href = item.getAttribute("href")
+
+    if (isURLValid(href) === false) {
+      return // skip
+    }
+
+    if (getKojiApp(href) === false) {
+      return // skip not Koji App link
+    }
+
+    item.addEventListener("click", (e) => {
+      e.preventDefault()
+      loadKojiApp(href)
+    })
+  })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  loadKojiButtons()
+  loadKojiLinks()
 })
