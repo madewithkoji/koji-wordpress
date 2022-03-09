c15e5b9d880758d1e2bdb6cc1d4888e284ddf77f
Victor Demin <demmbox@gmail.com>
2022-03-09 20:14:20 +0400

    koji editor white background

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Koji Block - Embed Block for Koji Apps
  * Plugin URI:        https://withkoji.com/
  * Description:       Easily embed Koji Apps in Wordpress posts. Supercharge your posts with e-commerce apps, games, and other types of powerful interactive posts.
- * Version:           1.0.1
+ * Version:           1.0.2
  */
 
 require __DIR__ . '/init.php';
